c061db073032628ac40f6e7fed8e4246b58df084
Samir-CP <130490699+Samir-CP@users.noreply.github.com>
2023-10-24 14:35:21 -0400

    initial commit

---
+++ datos.php
@@ -0,0 +1,7 @@
+<?php
+// Simula la generación de datos (en este caso, un arreglo de nombres)
+$datos = array("Ossian", "Marisol", "Erika", "Jairo", "Jeampier", "Lucy", "Maide", "Grover", "William", "Jenny");
+
+// Devuelve los datos en formato JSON
+echo json_encode($datos);
+?>
